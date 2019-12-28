bafc8f186e0dbd16e7abdb7d27e5430b96413057
Aman <amanansari.a2@gmail.com>
2019-12-28 14:49:29 +0530

    added navbar

--- app.js
+++ app.js
@@ -2,9 +2,76 @@
 
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+var campgrounds = [
+    {   
+        name:   "Robot",
+        img:    "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72287fd79249c459_340.png"
+    },
+    {
+        name:   "Funny Camp",
+        img:    "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72287fd79249c459_340.png"
+    },
+    {
+        name:   "Killing machine",
+        img :   "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72287fd79249c459_340.png"
+    },
+    {
+        name:   "abcd camp",
+        img :   "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72287fd79249c459_340.png"
+    },
+    {
+        name:   "xyz camp",
+        img :   "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72287fd79249c459_340.png"
+    },
+    {   
+        name:   "Robot",
+        img:    "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72287fd79249c459_340.png"
+    },
+    {
+        name:   "Funny Camp",
+        img:    "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72287fd79249c459_340.png"
+    },
+    {
+        name:   "Killing machine",
+        img :   "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72287fd79249c459_340.png"
+    },
+    {
+        name:   "abcd camp",
+        img :   "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72287fd79249c459_340.png"
+    },
+    {
+        name:   "xyz camp",
+        img :   "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72287fd79249c459_340.png"
+    },
+    {   
+        name:   "Robot",
+        img:    "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72287fd79249c459_340.png"
+    },
+    {
+        name:   "Funny Camp",
+        img:    "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72287fd79249c459_340.png"
+    },
+    {
+        name:   "Killing machine",
+        img :   "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72287fd79249c459_340.png"
+    },
+    {
+        name:   "abcd camp",
+        img :   "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72287fd79249c459_340.png"
+    },
+    {
+        name:   "xyz camp",
+        img :   "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72287fd79249c459_340.png"
+    }
+]
 
 app.set("view engine",'ejs');
-
+//app.use(express.static(__dirname + "/public/"));
 app.get('/',function (req,res) {  
     res.render('landing');
 });
@@ -12,25 +79,33 @@ app.get('/',function (req,res) {
 
 
 app.get('/campgrounds',function (req,res) {  
-    var campgrounds = [
-        {   
-            name:   "Robot",
-            img:    "/assets/Imgs/blake-carpenter-Ok2-7hSwYro-unsplash.jpg"
-        },
-        {
-            name:   "Funny",
-            img:    "/assets/Imgs/blake-carpenter-Ok2-7hSwYro-unsplash.jpg"
-        },
-        {
-            name:   "yeah yeah very funny",
-            img :   "/assets/Imgs/allison-christine-8j-2d94Orlc-unsplash.jpg"
-        }
-    ]
-
+ 
      
     res.render("campgrounds",{campgrounds:campgrounds});
 
     
+});
+var name,image;
+app.post('/campgrounds', (req, res) => {
+   
+  name = req.body.name;
+  image= req.body.image;
+  console.log(name);
+  console.log(image);
+
+    // get data from form and add to campgrounds array
+   var newCampG = {name:name, img : image};
+     campgrounds.push(newCampG);
+  
+    res.redirect("/campgrounds");
+ 
+    // redirect back to campgrounds page
+
+
+});
+
+app.get('/campgrounds/new',function (req, res){
+    res.render("new.ejs");
 });
 
 
