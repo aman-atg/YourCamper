43e64165bf8177bf8f70f984fca19eca0aa55f94
Aman <amanansari.a2@gmail.com>
2019-12-28 22:28:46 +0530

    Refactor the code

--- app.js
+++ app.js
@@ -1,106 +1,74 @@
+// ! img https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72287fd79249c459_340.png
 // ! why the fuck my shift + . not working sometimes 
 
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({extended:true}));
+var express     = require('express'),
+    app         = express(),
+    bodyParser  = require('body-parser'),
+    mongoose    = require('mongoose');
 
-
-var campgrounds = [
-    {   
-        name:   "Robot",
-        img:    "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72287fd79249c459_340.png"
-    },
-    {
-        name:   "Funny Camp",
-        img:    "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72287fd79249c459_340.png"
-    },
-    {
-        name:   "Killing machine",
-        img :   "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72287fd79249c459_340.png"
-    },
-    {
-        name:   "abcd camp",
-        img :   "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72287fd79249c459_340.png"
-    },
-    {
-        name:   "xyz camp",
-        img :   "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72287fd79249c459_340.png"
-    },
-    {   
-        name:   "Robot",
-        img:    "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72287fd79249c459_340.png"
-    },
-    {
-        name:   "Funny Camp",
-        img:    "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72287fd79249c459_340.png"
-    },
-    {
-        name:   "Killing machine",
-        img :   "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72287fd79249c459_340.png"
-    },
-    {
-        name:   "abcd camp",
-        img :   "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72287fd79249c459_340.png"
-    },
-    {
-        name:   "xyz camp",
-        img :   "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72287fd79249c459_340.png"
-    },
-    {   
-        name:   "Robot",
-        img:    "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72287fd79249c459_340.png"
-    },
-    {
-        name:   "Funny Camp",
-        img:    "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72287fd79249c459_340.png"
-    },
-    {
-        name:   "Killing machine",
-        img :   "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72287fd79249c459_340.png"
-    },
-    {
-        name:   "abcd camp",
-        img :   "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72287fd79249c459_340.png"
-    },
-    {
-        name:   "xyz camp",
-        img :   "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72287fd79249c459_340.png"
-    }
-]
-
+// ================================================ //
+app.use(bodyParser.urlencoded({extended:true}));  
 app.set("view engine",'ejs');
-//app.use(express.static(__dirname + "/public/"));
+//app.use(express.static(__dirname + "/public/")); // (for displaying local images from Images folder)
+// ================================================ //
+    mongoose.connect(
+    'mongodb://localhost/yelp_camp'
+    , {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true
+    });
+
+// ============== CAMPGROUND-SCHEMA ======================
+var campgroundSchema = new mongoose.Schema({
+    name        : String,
+    image       : String,
+    description : String
+});
+// ============= CAMPGROUND-MODEL ======================== 
+var Campground = mongoose.model("Campground",campgroundSchema)
+
+// =========================== >> DATABASE-MANAGEMENT << ==============================
+
+
+
+
+// =========================== >> END OF DB-MANAGEMENT << =============================
+
+// ============================ >>  ROUTES  << ========================================
+
 app.get('/',function (req,res) {  
     res.render('landing');
 });
 
-
-
 app.get('/campgrounds',function (req,res) {  
- 
-     
-    res.render("campgrounds",{campgrounds:campgrounds});
-
+    // Get all the campgrounds from DB
+    Campground.find({}, function (err,allCampgrounds){  
+        if(err)console.log(err);
+        else{
+            res.render("campgrounds",{campgrounds:allCampgrounds});
+        }
+    });
     
 });
-var name,image;
-app.post('/campgrounds', (req, res) => {
-   
-  name = req.body.name;
-  image= req.body.image;
-  console.log(name);
-  console.log(image);
-
-    // get data from form and add to campgrounds array
-   var newCampG = {name:name, img : image};
-     campgrounds.push(newCampG);
-  
-    res.redirect("/campgrounds");
  
-    // redirect back to campgrounds page
-
+app.post('/campgrounds',function(req, res){
+    // get data from form and add to campgrounds array 
+    var name = req.body.name;
+    var image= req.body.image;
+    console.log(name);
+    var newCampG = { name : name, image : image};
+    Campground.create(newCampG,function (err,newlyCreated) {  
+        if(err){
+            console.log("fuck");
+        }else
+        {   
+            console.log(newlyCreated);
+            res.redirect("/campgrounds");
+            // redirect back to campgrounds page
+        }
+    });
 
 });
 
@@ -108,7 +76,11 @@ app.get('/campgrounds/new',function (req, res){
     res.render("new.ejs");
 });
 
+// ============================ >> END OF ROUTES << ========================================
 
+
+
+// ============= LISTENING =============
 app.listen(3000,function(){
     console.log('listening to 3000 ...');
 });
