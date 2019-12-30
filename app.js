4ab8b2143872fe03c6796fb016e4d37065d8d7c9
Aman <amanansari.a2@gmail.com>
2019-12-31 04:13:56 +0530

    added new features

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ var express     = require('express'),
     bodyParser  = require('body-parser'),
     mongoose    = require('mongoose'),
     Campground  = require('./models/campground'),
-    seedDB      = require('./seeds'); seedDB();
-
+    seedDB      = require('./seeds'),
+    Comment     = require('./models/comment');
 // ================================================ //
 app.use(bodyParser.urlencoded({extended:true}));  
 app.set("view engine",'ejs');
+seedDB();
 //app.use(express.static(__dirname + "/public/")); // (for displaying local images from Images folder)
 // ================================================ //
     mongoose.connect(
@@ -35,7 +36,7 @@ app.get('/campgrounds',function (req,res) {
     Campground.find({}, function (err,allCampgrounds){  
         if(err)console.log(err);
         else{
-            res.render("index",{campgrounds:allCampgrounds});
+            res.render("campgrounds/index",{campgrounds:allCampgrounds});
         }
     });
     
@@ -63,7 +64,7 @@ app.post('/campgrounds',function(req, res){
 
 /* FORM FOR ADDING NEW CAMPGROUND */
 app.get('/campgrounds/new',function (req, res){
-    res.render("new.ejs");
+    res.render("campgrounds/new");
 });
 
 /*  Show Route */
@@ -74,7 +75,7 @@ app.get("/campgrounds/:id",(req,res)=>{
         if(err) console.log(err);
         else
         {   // showing the requested data
-            res.render("show",{campground:foundCampground});
+            res.render("campgrounds/show",{campground:foundCampground});
         } 
     });
   
@@ -82,11 +83,38 @@ app.get("/campgrounds/:id",(req,res)=>{
 
 /* New Comment form*/
 app.get('/campgrounds/:id/comments/new', (req, res) => {
-    res.render("show");
+    Campground.findById(req.params.id,(err, campground)=>{
+        if(err) console.log(err);
+        else
+        {
+            res.render("comments/new",{campground:campground});
+        }
+    });
+
 });
 
 /*  Create New Comment */
-
+app.post('/campgrounds/:id/comments', (req, res) => {
+    Campground.findById(req.params.id,(err, campground)=>{
+        if(err)
+        {
+            console.log(err);
+            res.redirect("/campgrounds");
+        } 
+        else
+        {
+            Comment.create(req.body.comment, (err,comment)=>{
+                if(err) console.log(err);
+                else{
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/"+campground._id)
+                }
+            });
+            
+        }
+    });
+});
 
 // ============================ >> END OF ROUTES << ========================================
 
