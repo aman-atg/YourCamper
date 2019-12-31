c62f2cf620ffa0c7e9a051c210236c1a4d4db7d1
Aman <amanansari.a2@gmail.com>
2019-12-31 15:16:54 +0530

    Some styling

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ var express     = require('express'),
 // ================================================ //
 app.use(bodyParser.urlencoded({extended:true}));  
 app.set("view engine",'ejs');
+app.use(express.static(__dirname + "/public"));
 seedDB();
-//app.use(express.static(__dirname + "/public/")); // (for displaying local images from Images folder)
 // ================================================ //
     mongoose.connect(
     'mongodb://localhost/yelp_camp'
--- seeds.js
+++ seeds.js
@@ -6,17 +6,17 @@ var data  = [
     {
         name        :   "Granite Hill",
         image       :   "https://www.photosforclass.com/download/pixabay-4363073?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72287ed6924ecd5f_960.png&user=bowl_of_nicole",
-        description :   "This is a nice description about the above fictional campground for pokemons and toy story fans....nyc...really nice"
+        description :   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas numquam inventore dolores? A voluptas, saepe ut delectus libero adipisci accusantium, illum voluptatem tempora rem corporis velit illo tempore quod unde. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio consequatur, est rerum, nostrum nulla ipsam asperiores adipisci explicabo amet ipsa voluptatum vel reprehenderit similique. Accusantium magni dolore vitae repellat ad. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet explicabo voluptatem minima placeat ex voluptate nobis necessitatibus harum ratione beatae doloremque, impedit, reprehenderit tempore quis, excepturi sint perferendis?At,vero!"
     },
     {
         name        :   "Granite Hill",
         image       :   "https://www.photosforclass.com/download/pixabay-4363073?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72287ed6924ecd5f_960.png&user=bowl_of_nicole",
-        description :   "This is a nice description about the above fictional campground for pokemons and toy story fans....nyc...really nice"
+        description :   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas numquam inventore dolores? A voluptas, saepe ut delectus libero adipisci accusantium, illum voluptatem tempora rem corporis velit illo tempore quod unde. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio consequatur, est rerum, nostrum nulla ipsam asperiores adipisci explicabo amet ipsa voluptatum vel reprehenderit similique. Accusantium magni dolore vitae repellat ad. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet explicabo voluptatem minima placeat ex voluptate nobis necessitatibus harum ratione beatae doloremque, impedit, reprehenderit tempore quis, excepturi sint perferendis?At,vero!"
     },
     {
         name        :   "Granite Hill",
         image       :   "https://www.photosforclass.com/download/pixabay-4363073?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72287ed6924ecd5f_960.png&user=bowl_of_nicole",
-        description :   "This is a nice description about the above fictional campground for pokemons and toy story fans....nyc...really nice"
+        description :   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas numquam inventore dolores? A voluptas, saepe ut delectus libero adipisci accusantium, illum voluptatem tempora rem corporis velit illo tempore quod unde. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio consequatur, est rerum, nostrum nulla ipsam asperiores adipisci explicabo amet ipsa voluptatum vel reprehenderit similique. Accusantium magni dolore vitae repellat ad. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet explicabo voluptatem minima placeat ex voluptate nobis necessitatibus harum ratione beatae doloremque, impedit, reprehenderit tempore quis, excepturi sint perferendis?At,vero!"
     }
 ];
 
